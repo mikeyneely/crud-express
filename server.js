13775b73da382f7e19b62a590756bbb7bd7e083d
Mikey Neely <mike.neely90@gmail.com>
2019-07-28 20:10:11 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,88 @@
+//all these variables are needed to access the modules and methods for the webapp
+//each module has its own methods that can be used
+//require is the method that comes with node.
+const express = require('express')
+const app = express()
+//body Parser pulls data from forms. "parse the body"
+const bodyParser = require('body-parser')
+//.mongoClient is a library
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+//url connects to mongo database
+const url = 'mongodb+srv://sirmikeyrocks:meleah22@crud1-68o08.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = "demo";
+//express method to listen to the localhost and a callback function to connect to the database/server
+app.listen(3000, () => {
+    MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+//what express does?
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+//everything in public folder is routed.
+
+
+
+//begin web app api
+app.get('/', (req, res) => {
+  //console.log(db)
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/countUp', (req, res) => {
+  db.collection('messages')
+  //findOneAndUpdate is a mongodb method. it goes through db, and collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.put('/countDown', (req, res) => {
+  db.collection('messages')
+  //findOneAndUpdate is a mongodb method. it goes through db, and collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbDown - 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/messages', (req, res) => {
+  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
